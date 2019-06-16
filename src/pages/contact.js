14a82a5b95d452a50372a1014e6cf6b5d7559edf
andris.svarcs <andris.svarcs@yahoo.com>
2019-06-16 09:52:51 +0100

    input field for form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,17 +2,18 @@ import React from "react"
 import { Header } from "../components/header/Header"
 import { Container } from "../templates/container/Container"
 import { AxiosInstance as Axios } from "axios"
+import firebase from "firebase/app"
 
-const handleSubmit = event => {
+const handleSubmit = (event) => {
   event.preventDefault()
-
-  const data = {
+  console.log("event: ", event)
+  /*const data = {
     email,
     message,
     time: getTime(),
-  }
+  }*/
 
-  Axios.post("https://us-central1-ourprojectname.cloudfunctions.net/submit", data)
+  /*Axios.post("https://us-central1-ourprojectname.cloudfunctions.net/submit", data)
     .then(res => {
       if (firebase) {
         return firebase
@@ -23,30 +24,70 @@ const handleSubmit = event => {
     })
     .catch(error => {
       console.log(error)
-    })
+    })*/
 }
 
-export default () => (
-  <Container>
-    <Header
-      headerText='Contact us'
-    />
-    <h3>Contact form</h3>
-    <form onSubmit={handleSubmit}>
-      <label>
-        e-mail:
-        <br/>
-        <input type="email" name="email" value="{email}" onChange={handleEmailChange}/>
-      </label>
-      <br/>
-      <label>
-        Message
-        <br/>
-        <textarea type="text" name="message" value="{message}" onChange={handleMessageChange}/>
-      </label>
-      <br/>
-      <input type="submit" value="submit"/>
-    </form>
-  </Container>
-);
+const handleMessageChange = (e) => {
+  console.log("handleMessageChange: ", e)
+}
+
+export default class Contact extends React.Component {
+  state = {
+    email: "",
+  }
+
+  componentDidMount() {
+    console.log("Component did mount", firebase)
+  }
+
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    })
+  }
+
+  render() {
+    console.log(this)
+    const {
+            email,
+          } = this.state
+    return (
+      <Container>
+        <Header
+          headerText='Contact us'
+        />
+        <h3>Contact form</h3>
+        <form
+          onSubmit={handleSubmit}
+        >
+          <label>
+            e-mail:
+            <br/>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={this.handleEmailChange}
+            />
+          </label>
+          <br/>
+          <label>
+            Message
+            <br/>
+            <textarea
+              name="message"
+              value="{message}"
+              onChange={handleMessageChange}
+            />
+          </label>
+          <br/>
+          <input
+            type="submit"
+            value="submit"
+          />
+        </form>
+      </Container>
+    )
+  }
+};
 
