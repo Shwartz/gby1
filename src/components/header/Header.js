0061ead5568d567fc40236bc01651783d779e3d0
andris.svarcs <andris.svarcs@yahoo.com>
2019-08-12 18:06:18 +0300

    Making header as stand alone component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,39 +1,21 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import { Navigation } from "../navigation/Navigation"
+import Img from "gatsby-image"
 import styles from "./Header.module.scss"
 
-const mainMenuDefinition = [
-  { "Home": "/" },
-  { "Blog": "/blog" },
-  { "About": "/about" },
-  { "Contact us": "/contact" },
-]
-
-const mainMenu = (arr) => {
-  return arr.map((obj, index) => {
-    const pageName = Object.keys(obj)[0]
-    const slug     = obj[pageName]
-
-    return (
-      <Link
-        key={index}
-        to={slug}
-        activeClassName={styles.active}
-      >{pageName}
-      </Link>
-    )
-  })
+export const Header = (props) => {
+  console.log('data header: ', props);
+  return (
+    <div className={styles.header}>
+      <Img
+      fixed={props.logo.childImageSharp.fixed}
+      alt="Migraine Detective"
+    />
+      <Navigation/>
+    </div>
+  )
 }
-
-export const Header = (props) => (
-  <div className={styles.header}>
-    <h1>{props.headerText}</h1>
-    <nav className={styles.menu}>
-      {mainMenu(mainMenuDefinition)}
-    </nav>
-  </div>
-)
 
 Header.propTypes = {
   headerText: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,35 @@
-import React from "react";
-import { Navigation } from "../components/navigation/Navigation";
-import { Container } from "../templates/container/Container";
-import Img from "gatsby-image";
-import { graphql } from "gatsby";
+import React from "react"
+import { Container } from "../templates/container/Container"
+import { graphql } from "gatsby"
+import { Header } from "../components/header/Header"
 
 export default ({ data }) => {
+  console.log("data: ", data)
+
   return (
     <Container>
-      <Img fixed={data.file.childImageSharp.fixed} />
-      <Navigation />
+      <div>
+        <Header
+          logo={data.imgLogo}
+        />
+        <p>Meta data from graphQL: {data.site.siteMetadata.title}</p>
 
-      <p>Meta data from graphQL: {data.site.siteMetadata.title}</p>
+        <h3>Yar Pirate Ipsum</h3>
 
-      <h3>Yar Pirate Ipsum</h3>
+        <p>Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast
+          quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon
+          starboard
+          grog black jack gangway rutters.</p>
 
-      <p>Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast
-        quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard
-        grog black jack gangway rutters.</p>
+        <p>Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a
+          spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry
+          doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.</p>
 
-      <p>Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a
-        spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry
-        doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.</p>
-
-      <p>Trysail Sail ho Corsair red ensign hulk smartly boom jib rum gangway. Case shot Shiver me timbers gangplank
-        crack Jennys tea cup ballast Blimey lee snow crow's nest rutters. Fluke jib scourge of the seven seas boatswain
-        schooner gaff booty Jack Tar transom spirits.</p>
+        <p>Trysail Sail ho Corsair red ensign hulk smartly boom jib rum gangway. Case shot Shiver me timbers gangplank
+          crack Jennys tea cup ballast Blimey lee snow crow's nest rutters. Fluke jib scourge of the seven seas
+          boatswain
+          schooner gaff booty Jack Tar transom spirits.</p>
+      </div>
     </Container>
   )
 }
@@ -36,7 +41,7 @@ export const query = graphql`
         title
       }
     }
-    file(relativePath: { eq: "icons/Logo.png" }) {    
+    imgLogo: file(relativePath: { eq: "icons/Logo.png" }) {    
       childImageSharp {
         # Specify the image processing specifications right in the query.
         # Makes it trivial to update as your page's design changes.    
