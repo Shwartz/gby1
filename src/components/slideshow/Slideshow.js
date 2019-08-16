3902c6ffa80dc702b26e63448746469df4f4af1f
andris.svarcs <andris.svarcs@yahoo.com>
2019-08-16 21:52:27 +0300

    Removed image flickering between screens for a Slide

--- src/components/slideshow/Slideshow.js
+++ src/components/slideshow/Slideshow.js
@@ -1,20 +1,35 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useReducer } from "react"
 import { useInterval } from "../../helpers/useInterval"
 import Img from "gatsby-image"
 import styles from "./Slideshow.module.scss"
 
+const initialState = {
+  count:        0,
+  animateCount: 0,
+  step:         0,
+}
+
 export const Slideshow = (props) => {
-  const [count, setCount]               = useState(0)
-  const [animateCount, setAnimateCount] = useState(0)
-  const [animClass, setAnimClass]       = useState([])
-  const delay                           = 2000
+  const [state, setState] = useReducer(
+    // reducer function that will work the same way as React's setState function
+    (oldState, newState) => ({ ...oldState, ...newState }),
+    // initial state
+    {
+      ...initialState,
+    },
+  )
+
+  /*  const [count, setCount]               = useState(0)
+    const [animateCount, setAnimateCount] = useState(0)
+    const [step, setStep]                 = useState(0)*/
+  const delay    = 5000
   const {
           imgHero,
           imgSlide1,
           imgSlide2,
           imgSlide3,
-        }                               = props.images
-  const slideArr                        = [imgSlide1, imgSlide2, imgSlide3].map((slide) => {
+        }        = props.images
+  const slideArr = [imgSlide1, imgSlide2, imgSlide3].map((slide) => {
     return (
       <Img
         fixed={slide.childImageSharp.fixed}
@@ -23,44 +38,95 @@ export const Slideshow = (props) => {
     )
   })
 
-  const imgCounter = count => count > 2 ? 0 : count
+  const imgCounter = count => count > slideArr.length ? 0 : count
 
   const runSlide = () => {
-    //console.log('runSlide getIndex(index): ', getIndex(index));
-    setAnimateCount(count % 3)
-
-    setAnimClass([styles.slide])
-
-    setTimeout(() => {
-      setAnimClass([styles.slide, styles.slideOut])
-      setTimeout(() => {
-        setAnimClass([styles.slide, styles.hide])
-      }, 400)
-    }, delay - 1000)
-
-    console.log("count % 3: ", count % 3)
+    setTimeout(() => setState({ step: 1 }), 500)
+    setTimeout(() => setState({ step: 2 }), 900)
+    setTimeout(() => setState({ step: 3 }), 1300)
+    setTimeout(() => setState({ step: 4 }), 1800)
   }
 
   useInterval(() => {
     // Interval still runs but won't execute method. Interval will stop once component is removed
+    const { count, animateCount } = state
     if (count < 10) {
-      setCount(count + 1)
+      setState(
+        {
+          step:          1,
+          count:         count + 1,
+          animateCount: count % 3,
+        },
+      )
+
+      console.log("--- count, animatedCount", count, animateCount)
       runSlide()
     }
   }, delay)
 
-  const imgChanger = (count) => {
+  const slide1 = (aCounts, step) => {
+    console.log("slide1 aCounts - step: ", aCounts, step)
+    /*let dom = ""
+    if (step === 1) {
+      dom = slideArr[imgCounter(aCounts + 1)]
+    }
+    if (step === 2) {
+      dom = slideArr[imgCounter(aCounts + 1)]
+    }
+    if (step === 3 || step === 4) {
+      dom = slideArr[imgCounter(aCounts + 1)]
+    }*/
     return (
       <React.Fragment>
         <div className={`${styles.slide}`}>
-          {slideArr[imgCounter(count + 1)]}
-        </div>
-        <div className={`${animClass.join(' ')}`}>
-          {slideArr[imgCounter(count)]}
+          {slideArr[imgCounter(aCounts + 1)]}
         </div>
       </React.Fragment>
     )
   }
+
+  const slide2 = (aCounts, step) => {
+    console.log("slide2 aCounts - step: ", aCounts, step)
+    let dom = ""
+    if (step === 1) {
+      dom = (
+        <div className={`${styles.slide}`}>
+          {slideArr[imgCounter(aCounts)]}
+        </div>
+      )
+    }
+    if (step === 2) {
+      dom = (
+        <div className={`${styles.slide} ${styles.slideOut}`}>
+          {slideArr[imgCounter(aCounts)]}
+        </div>
+      )
+    }
+    if (step === 3) {
+      dom = (
+        <div className={`${styles.slide} ${styles.hide}`}>
+          {slideArr[imgCounter(aCounts + 1)]}
+        </div>
+      )
+    }
+    if (step === 4) {
+      dom = (
+        <div className={`${styles.slide}`}>
+          {slideArr[imgCounter(aCounts + 1)]}
+        </div>
+      )
+    }
+    return (
+      <React.Fragment>
+        {dom}
+      </React.Fragment>
+    )
+  }
+
+  const {
+          animateCount,
+          step,
+        } = state
 
   return (
     <div>
@@ -71,10 +137,8 @@ export const Slideshow = (props) => {
           alt="Migraine Detective"
         />
         <div className={styles.wrap}>
-          <div className={`${styles.slide}`}>
-            {slideArr[imgCounter(count + 1)]}
-          </div>
-          {imgChanger(animateCount)}
+          {slide1(animateCount, step)}
+          {slide2(animateCount, step)}
         </div>
       </div>
     </div>
