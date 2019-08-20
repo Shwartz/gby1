e0c814f801f7498077306eaaa5af5ed36ac8fea5
andris.svarcs <andris.svarcs@yahoo.com>
2019-08-20 08:20:49 +0300

    Animation when element is in viewport

--- src/components/home/readMore/ReadMore.js
+++ src/components/home/readMore/ReadMore.js
@@ -3,20 +3,16 @@ import styles from "./ReadMore.module.scss"
 import { useInView } from "react-intersection-observer"
 
 const ReadMore = (props) => {
-  const [ref, inView, entry] = useInView({
-                                           /* Optional options */
-                                           threshold: 1,
-                                         })
+  const [ref1, inView1] = useInView({ threshold: 1 })
+  const [ref2, inView2] = useInView({ threshold: 1 })
+  const [ref3, inView3] = useInView({ threshold: 1 })
 
   return (
     <div className={styles.wrap}>
       <section>
         <div>
           <h3>Talk to your doctor</h3>
-          <div ref={ref}>
-            <h2>{`Header inside viewport ${inView}.`}</h2>
-          </div>
-          <span className={styles.animate}/>
+          <span ref={ref1} className={inView1 ? styles.animate : ""}/>
           <p>A certified neurologist will give a proper migraine diagnosis. First of all, she will determine whether you
             have primary or secondary migraines. If you are being diagnosed with chronic migraine or headaches, she may
             recommend you writing a headache diary to identify the triggers.</p>
@@ -43,6 +39,7 @@ const ReadMore = (props) => {
         </div>
         <div>
           <h3>Keep a headache diary</h3>
+          <span ref={ref2} className={inView2 ? styles.animate : ""}/>
           <p>Track your lifestyle events as well as migraine episodes and medicines with Migraine Detective. The more
             you add to the diary, the better chances are that you will be able to identify patterns that trigger your
             migraine episodes.</p>
@@ -53,6 +50,7 @@ const ReadMore = (props) => {
       <section>
         <div>
           <h3>Get meaningful insights</h3>
+          <span ref={ref3} className={inView3 ? styles.animate : ""}/>
           <p>Identify patterns of events that trigger your migraines with advanced reporting features. Migraine
             Detective provides you with meaningful data points that you can share with your doctor and support your
             conversation.​​</p>
