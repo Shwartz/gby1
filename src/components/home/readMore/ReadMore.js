7b8317b33e8b05737411adb9be984df3f9abaeea
andris.svarcs <andris.svarcs@yahoo.com>
2019-09-07 13:27:54 +0100

    Read more section max width limit

--- src/components/home/readMore/ReadMore.js
+++ src/components/home/readMore/ReadMore.js
@@ -10,64 +10,66 @@ const ReadMore = (props) => {
 
   return (
     <div className={styles.wrap}>
-      <section>
-        <div>
-          <em>01</em>
-          <ul>
-            <li>Primary or secondary headaches?</li>
-            <li>Writing a headache diary</li>
-            <li>Identifying migraine triggers</li>
-          </ul>
-        </div>
-        <div>
-          <h3>Talk to your doctor</h3>
-          <span ref={ref1} className={inView1 ? styles.animate : ""}/>
-          <p>A certified neurologist will give a proper migraine diagnosis. First of all, she will determine whether you
-            have primary or secondary migraines. If you are being diagnosed with chronic migraine or headaches, she may
-            recommend you writing a headache diary to identify the triggers.</p>
+      <div className={styles.content}>
+        <section>
+          <div>
+            <em>01</em>
+            <ul>
+              <li>Primary or secondary headaches?</li>
+              <li>Writing a headache diary</li>
+              <li>Identifying migraine triggers</li>
+            </ul>
+          </div>
+          <div>
+            <h3>Talk to your doctor</h3>
+            <span ref={ref1} className={inView1 ? styles.animate : ""}/>
+            <p>A certified neurologist will give a proper migraine diagnosis. First of all, she will determine whether
+              you
+              have primary or secondary migraines. If you are being diagnosed with chronic migraine or headaches, she
+              may
+              recommend you writing a headache diary to identify the triggers.</p>
 
-          <p><Link to="/blog/what-are-migraine-triggers">What are migraine triggers?</Link></p>
-        </div>
-      </section>
-      <section>
-        <div>
-          <em>02</em>
-          <ul>
-            <li>Migraine and lifestyle diary</li>
-            <li>More than 12 diary categories</li>
-            <li>Flexible tagging</li>
-          </ul>
-        </div>
-        <div>
-          <h3>Keep a headache diary</h3>
-          <span ref={ref2} className={inView2 ? styles.animate : ""}/>
-          <p>Track your lifestyle events as well as migraine episodes and medicines with Migraine Detective. The more
-            you add to the diary, the better chances are that you will be able to identify patterns that trigger your
-            migraine episodes.</p>
+            <p><Link to="/blog/what-are-migraine-triggers">What are migraine triggers?</Link></p>
+          </div>
+        </section>
+        <section>
+          <div>
+            <em>02</em>
+            <ul>
+              <li>Migraine and lifestyle diary</li>
+              <li>More than 12 diary categories</li>
+              <li>Flexible tagging</li>
+            </ul>
+          </div>
+          <div>
+            <h3>Keep a headache diary</h3>
+            <span ref={ref2} className={inView2 ? styles.animate : ""}/>
+            <p>Track your lifestyle events as well as migraine episodes and medicines with Migraine Detective. The more
+              you add to the diary, the better chances are that you will be able to identify patterns that trigger your
+              migraine episodes.</p>
 
-          <p><Link to="/blog/migraine-diary-examples">View migraine diary examples</Link>
-          </p>
-        </div>
-      </section>
-      <section>
-        <div>
-          <em>03</em>
-          <ul>
-            <li>Frequency of migraine episodes</li>
-            <li>Details about medication intakes</li>
-            <li>Patterns of events triggering migraines</li>
-          </ul>
-        </div>
-        <div>
-          <h3>Get meaningful insights</h3>
-          <span ref={ref3} className={inView3 ? styles.animate : ""}/>
-          <p>Identify patterns of events that trigger your migraines with advanced reporting features. Migraine
-            Detective provides you with meaningful data points that you can share with your doctor and support your
-            conversation.​​</p>
-        </div>
-      </section>
-
-
+            <p><Link to="/blog/migraine-diary-examples">View migraine diary examples</Link>
+            </p>
+          </div>
+        </section>
+        <section>
+          <div>
+            <em>03</em>
+            <ul>
+              <li>Frequency of migraine episodes</li>
+              <li>Details about medication intakes</li>
+              <li>Patterns of events triggering migraines</li>
+            </ul>
+          </div>
+          <div>
+            <h3>Get meaningful insights</h3>
+            <span ref={ref3} className={inView3 ? styles.animate : ""}/>
+            <p>Identify patterns of events that trigger your migraines with advanced reporting features. Migraine
+              Detective provides you with meaningful data points that you can share with your doctor and support your
+              conversation.​​</p>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
