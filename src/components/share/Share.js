8ec64bafa474dc18232a86175bcf082404d1fe9f
andris.svarcs <andris.svarcs@yahoo.com>
2019-09-10 08:13:08 +0100

    Working state for Share Icons with basic styling and count

--- src/components/share/Share.js
+++ src/components/share/Share.js
@@ -1,33 +1,66 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import { fac } from '@fortawesome/free-solid-svg-icons'
+import React from "react"
+
 import {
   FacebookShareButton,
   LinkedinShareButton,
   TwitterShareButton,
-} from 'react-share';
+  FacebookShareCount,
+  FacebookIcon,
+  TwitterIcon
+} from "react-share"
 
-import styles from './Share.module.scss';
+import "./Share.scss"
 
 const Share = ({ socialConfig, tags }) => {
   console.log("socialConfig: ", socialConfig)
   console.log("tags: ", tags)
-  const fb = styles.facebook;
+
+  const fbShareCount = (
+    <FacebookShareCount url='https://migraine-detective.com'>
+      {shareCount => {
+        return (
+          <span className="myShareCountWrapper">{shareCount}</span>
+        )
+      }}
+    </FacebookShareCount>
+  );
 
   return (
     <div className="post-social">
-      <FacebookShareButton url={socialConfig.config.url} className="button is-outlined is-rounded facebook" >
+      <hr/>
+      <FacebookShareButton url="https://migraine-detective.com">
+        <FacebookIcon size={32} round={true}/>
+        {fbShareCount}
+      </FacebookShareButton>
+
+      <TwitterShareButton
+        title="The digital diary which helps to find triggers for your migraine"
+        url="https://migraine-detective.com"
+        via={socialConfig.twitterHandle.split('@').join('')}
+        hashtags={['migraine', 'diary', 'headache']}
+      >
+        <TwitterIcon size={32} round={true}/>
+      </TwitterShareButton>
+
+      <hr/>
+
+      {/*<FacebookShareButton url={socialConfig.config.url} className="button is-outlined is-rounded facebook" >
         <span className="icon">
-          <FontAwesomeIcon icon={['fab', 'facebook-f']} />
+          <FontAwesomeIcon icon={faFacebookF} />
         </span>
         <span className="text">Facebook</span>
-      </FacebookShareButton>
-      <TwitterShareButton url={socialConfig.config.url} className="button is-outlined is-rounded twitter" title={socialConfig.config.title} via={socialConfig.twitterHandle.split('@').join('')} hashtags={tags} >
+        <FacebookShareCount url={socialConfig.config.url}>
+          {shareCount => (
+            <span className="myShareCountWrapper">{shareCount}</span>
+          )}
+        </FacebookShareCount>
+      </FacebookShareButton>*/}
+      {/*<TwitterShareButton url={socialConfig.config.url} className="button is-outlined is-rounded twitter" title={socialConfig.config.title} via={socialConfig.twitterHandle.split('@').join('')} hashtags={tags} >
         <span className="icon">
           <FontAwesomeIcon icon={['fab', 'twitter']} />
         </span>
         <span className="text">Twitter</span>
-      </TwitterShareButton>
+      </TwitterShareButton>*/}
       {/*<LinkedinShareButton url={socialConfig.config.url} className="button is-outlined is-rounded linkedin" title={socialConfig.config.title} >
 			<span className="icon">
 				<FontAwesomeIcon icon={['fab', 'linkedin-in']} />
@@ -36,10 +69,31 @@ const Share = ({ socialConfig, tags }) => {
     </LinkedinShareButton>*/}
     </div>
   )
-};
+}
 
-export default Share;
+export default Share
 
 /*
 * INFO: https://swas.io/blog/social-share-button-for-gatsby-blog-pages/
+*
+* react-share module changes in file: FacebookShareCount.js (node_modules)
+*
+* import jsonp from 'jsonp';
+
+import shareCountFactory from './utils/shareCountFactory';
+
+function getFacebookShareCount(shareUrl, callback) {
+  //var endpoint = 'https://graph.facebook.com/?id=' + shareUrl;
+  var endpoint = 'https://graph.facebook.com/v2.2/?id=' + shareUrl + '&fields=og_object{engagement}';
+
+  jsonp(endpoint, function (err, data) {
+     callback(!err && data && data.share && data.share.share_count ? data.share.share_count : undefined);
+     callback(!err && data && data.og_object && data.og_object.engagement && data.og_object.engagement.count
+                ? data.og_object.engagement.count
+                : undefined
+     );
+  });
+}
+
+export default shareCountFactory(getFacebookShareCount);
 * */
