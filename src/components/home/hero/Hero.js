83f311c69307a0634e878a9a63d6a8b368a9b2f8
andris.svarcs <andris.svarcs@yahoo.com>
2019-09-24 06:59:32 +0100

    Basement for Parallax

--- src/components/home/hero/Hero.js
+++ src/components/home/hero/Hero.js
@@ -1,24 +1,32 @@
-import React from "react"
-import { WhiteBlueButton } from "../../buttons/Buttons"
-import styles from "./Hero.module.scss"
-import { Slideshow } from "../../slideshow/Slideshow"
+import React from 'react';
+import {WhiteBlueButton} from '../../buttons/Buttons';
+import styles from './Hero.module.scss';
+import {Slideshow} from '../../slideshow/Slideshow';
 
 export const Hero = (props) => {
 
-  return (
-    <section className={styles.hero}>
-      <div className={styles.heroWrap}>
-        <h1>Migraine Detective is a digital headache diary that helps to find your migraine triggers.</h1>
-        <WhiteBlueButton
-          href="https://migraine-detective.com/open-diary"
-        >
-          Open your diary
-        </WhiteBlueButton>
-        <Slideshow
-          images={props.images}
-        />
-      </div>
-    </section>
-  )
-}
+    return (
+        <section className={styles.hero}>
+            <div className={styles.heroWrap}>
+                <h1>Migraine Detective is a digital headache diary that helps to find your migraine triggers.</h1>
+                <WhiteBlueButton
+                    href="https://migraine-detective.com/open-diary"
+                >
+                    Open your diary
+                </WhiteBlueButton>
+                <Slideshow
+                    images={props.images}
+                />
+            </div>
+            <div className="parallax">
+                <div className="parallax__layer parallax__layer--back">
+                    <p>this is back</p>
+                </div>
+                <div className="parallax__layer parallax__layer--base">
+                    <p>This is front</p>
+                </div>
+            </div>
+        </section>
+    );
+};
 
