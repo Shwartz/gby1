307414f1c35fc46cba9969074ed4e8c2803f02e0
andris.svarcs <andris.svarcs@yahoo.com>
2019-09-25 22:02:32 +0100

    Positioning Hero Slide pixel precise

--- src/components/slideshow/Slideshow.js
+++ src/components/slideshow/Slideshow.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useReducer } from "react"
-import { useInterval } from "../../helpers/useInterval"
-import Img from "gatsby-image"
-import styles from "./Slideshow.module.scss"
+import React, {useState, useEffect, useReducer} from 'react';
+import {useInterval} from '../../helpers/useInterval';
+import Img from 'gatsby-image';
+import styles from './Slideshow.module.scss';
 
 /**
  *
@@ -10,126 +10,126 @@ import styles from "./Slideshow.module.scss"
  * TODO: add meaningful alt tags for a slide images
  */
 const initialState = {
-  count:       0,
-  imageToShow: 0,
-  step:        1,
-}
+    count:       0,
+    imageToShow: 0,
+    step:        1,
+};
 
 export const Slideshow = (props) => {
-  const [state, setState] = useReducer(
-    // reducer function that will work the same way as React's setState function
-    (oldState, newState) => ({ ...oldState, ...newState }),
-    // initial state
-    {
-      ...initialState,
-    },
-  )
-
-  const delay    = 3000
-  const {
-          imgHero,
-          imgSlide1,
-          imgSlide2,
-          imgSlide3,
-        }        = props.images
-  const slideArr = [imgSlide1, imgSlide2, imgSlide3].map((slide) => {
-    return (
-      <Img
-        fixed={slide.childImageSharp.fixed}
-        alt="Migraine Detective"
-      />
-    )
-  })
-
-  useEffect(() => {
-    runSlide()
-  }, [])
-
-  const imgCounter = count => count > (slideArr.length - 1) ? 0 : count
-
-  const runSlide = () => {
-    setTimeout(() => setState({ step: 1 }), 600)
-    setTimeout(() => setState({ step: 2 }), 900)
-    setTimeout(() => setState({ step: 3 }), 1300)
-    setTimeout(() => setState({ step: 4 }), 1800)
-  }
-
-  useInterval(() => {
-    // Interval still runs but won't execute method. Interval will stop once component is removed
-    const { count } = state
-    if (count < 16) {
-      const nextCount       = count + 1
-      const nextImageToShow = nextCount % slideArr.length
-
-      setState(
+    const [state, setState] = useReducer(
+        // reducer function that will work the same way as React's setState function
+        (oldState, newState) => ({...oldState, ...newState}),
+        // initial state
         {
-          step:        1,
-          count:       nextCount,
-          imageToShow: nextImageToShow,
+            ...initialState,
         },
-      )
-      runSlide()
-    }
-  }, delay)
+    );
 
-  const slide1 = (aCounts) => {
+    const delay    = 3000;
+    const {
+              imgHero,
+              imgSlide1,
+              imgSlide2,
+              imgSlide3,
+          }        = props.images;
+    const slideArr = [imgSlide1, imgSlide2, imgSlide3].map((slide) => {
+        return (
+            <Img
+                fixed={slide.childImageSharp.fixed}
+                alt="Migraine Detective"
+            />
+        );
+    });
+
+    useEffect(() => {
+        runSlide();
+    }, []);
+
+    const imgCounter = count => count > (slideArr.length - 1) ? 0 : count;
+
+    const runSlide = () => {
+        setTimeout(() => setState({step: 1}), 600);
+        setTimeout(() => setState({step: 2}), 900);
+        setTimeout(() => setState({step: 3}), 1300);
+        setTimeout(() => setState({step: 4}), 1800);
+    };
+
+    useInterval(() => {
+        // Interval still runs but won't execute method. Interval will stop once component is removed
+        const {count} = state;
+        if (count < 16) {
+            const nextCount       = count + 1;
+            const nextImageToShow = nextCount % slideArr.length;
+
+            setState(
+                {
+                    step:        1,
+                    count:       nextCount,
+                    imageToShow: nextImageToShow,
+                },
+            );
+            runSlide();
+        }
+    }, delay);
+
+    const slide1 = (aCounts) => {
+        return (
+            <div className={`${styles.slide}`}>
+                {slideArr[imgCounter(aCounts + 1)]}
+            </div>
+        );
+    };
+
+    const slide2 = (aCounts, step) => {
+        let dom = '';
+        if (step === 1) {
+            dom = (
+                <div className={`${styles.slide}`}>
+                    {slideArr[imgCounter(aCounts)]}
+                </div>
+            );
+        }
+        if (step === 2) {
+            dom = (
+                <div className={`${styles.slide} ${styles.slideOut}`}>
+                    {slideArr[imgCounter(aCounts)]}
+                </div>
+            );
+        }
+        if (step === 3) {
+            dom = (
+                <div className={`${styles.slide} ${styles.hide}`}>
+                    {slideArr[imgCounter(aCounts + 1)]}
+                </div>
+            );
+        }
+        if (step === 4) {
+            dom = (
+                <div className={`${styles.slide}`}>
+                    {slideArr[imgCounter(aCounts + 1)]}
+                </div>
+            );
+        }
+        return (
+            dom
+        );
+    };
+
+    const {
+              imageToShow,
+              step,
+          } = state;
+
     return (
-        <div className={`${styles.slide}`}>
-          {slideArr[imgCounter(aCounts + 1)]}
+        <div className={styles.phone}>
+            <Img
+                fixed={imgHero.childImageSharp.fixed}
+                alt="Migraine Detective"
+            />
+            <div className={styles.wrap}>
+                {slide1(imageToShow)}
+                {slide2(imageToShow, step)}
+            </div>
         </div>
-    )
-  }
-
-  const slide2 = (aCounts, step) => {
-    let dom = ""
-    if (step === 1) {
-      dom = (
-        <div className={`${styles.slide}`}>
-          {slideArr[imgCounter(aCounts)]}
-        </div>
-      )
-    }
-    if (step === 2) {
-      dom = (
-        <div className={`${styles.slide} ${styles.slideOut}`}>
-          {slideArr[imgCounter(aCounts)]}
-        </div>
-      )
-    }
-    if (step === 3) {
-      dom = (
-        <div className={`${styles.slide} ${styles.hide}`}>
-          {slideArr[imgCounter(aCounts + 1)]}
-        </div>
-      )
-    }
-    if (step === 4) {
-      dom = (
-        <div className={`${styles.slide}`}>
-          {slideArr[imgCounter(aCounts + 1)]}
-        </div>
-      )
-    }
-    return (
-      dom
-    )
-  }
-
-  const {
-          imageToShow,
-          step,
-        } = state
-
-  return (
-      <div className={styles.phone}>
-        <Img
-          fixed={imgHero.childImageSharp.fixed}
-          alt="Migraine Detective"
-        />
-        <div className={styles.wrap}>
-          {slide1(imageToShow)}
-          {slide2(imageToShow, step)}
-        </div>
-      </div>
-  )
-}
+    );
+};
