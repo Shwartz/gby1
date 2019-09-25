9a6048f34ebba2fd88d7ea85c111a5f4f80774b8
andris.svarcs <andris.svarcs@yahoo.com>
2019-09-25 08:05:52 +0100

    Adding Parallax scroll lib

--- src/components/home/hero/Hero.js
+++ src/components/home/hero/Hero.js
@@ -2,31 +2,37 @@ import React from 'react';
 import {WhiteBlueButton} from '../../buttons/Buttons';
 import styles from './Hero.module.scss';
 import {Slideshow} from '../../slideshow/Slideshow';
+import {ParallaxProvider, Parallax} from 'react-scroll-parallax';
+
+const ParallaxImage1 = ({img}) => {
+    console.log('--- img: ', img);
+    console.log('--- img.imgLogo....: ', img.imgLogo);
+    return <Parallax className={styles.parallaxImg1} y={[-350, 150]} tagOuter="figure">
+        <img src={img.imgLogo.childImageSharp.fixed.src} />
+    </Parallax>;
+};
 
 export const Hero = (props) => {
-
+    /* https://www.npmjs.com/package/react-scroll-parallax */
+    console.log("props: ", props);
+    console.log('logo: ', props.images.imgLogo.childImageSharp.fixed.src);
     return (
-        <section className={styles.hero}>
-            <div className={styles.heroWrap}>
-                <h1>Migraine Detective is a digital headache diary that helps to find your migraine triggers.</h1>
-                <WhiteBlueButton
-                    href="https://migraine-detective.com/open-diary"
-                >
-                    Open your diary
-                </WhiteBlueButton>
-                <Slideshow
-                    images={props.images}
-                />
-            </div>
-            <div className="parallax">
-                <div className="parallax__layer parallax__layer--back">
-                    <p>this is back</p>
+        <ParallaxProvider>
+            <section className={styles.hero}>
+                <div className={styles.heroWrap}>
+                    <h1>Migraine Detective is a digital headache diary that helps to find your migraine triggers.</h1>
+                    <WhiteBlueButton
+                        href="https://migraine-detective.com/open-diary"
+                    >
+                        Open your diary
+                    </WhiteBlueButton>
+                    <Slideshow
+                        images={props.images}
+                    />
                 </div>
-                <div className="parallax__layer parallax__layer--base">
-                    <p>This is front</p>
-                </div>
-            </div>
-        </section>
+                <ParallaxImage1 img={props.images} />
+            </section>
+        </ParallaxProvider>
     );
 };
 
