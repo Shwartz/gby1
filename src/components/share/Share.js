4ffe18bca6f37e932d1d924056a94a66bbc57202
andris.svarcs <andris.svarcs@yahoo.com>
2019-10-05 21:00:16 +0100

    Page format

--- src/components/share/Share.js
+++ src/components/share/Share.js
@@ -1,57 +1,57 @@
-import React from "react"
+import React from 'react';
 
 import {
-  FacebookShareButton,
-  LinkedinShareButton,
-  TwitterShareButton,
-  FacebookShareCount,
-  FacebookIcon,
-  TwitterIcon,
-} from "react-share"
+    FacebookShareButton,
+    LinkedinShareButton,
+    TwitterShareButton,
+    FacebookShareCount,
+    FacebookIcon,
+    TwitterIcon,
+} from 'react-share';
 
-import "./Share.scss"
+import './Share.scss';
 
-const Share = ({ socialConfig }) => {
-  const {title, url, hashTags, twitterHandle} = socialConfig;
-  const fbShareCount = (
-    <FacebookShareCount url='https://migraine-detective.com'>
-      {shareCount => {
-        return (
-          <span className="myShareCountWrapper">{shareCount ? shareCount : null}</span>
-        )
-      }}
-    </FacebookShareCount>
-  )
+const Share = ({socialConfig}) => {
+    const {title, url, hashTags, twitterHandle} = socialConfig;
+    const fbShareCount                          = (
+        <FacebookShareCount url='https://migraine-detective.com'>
+            {shareCount => {
+                return (
+                    <span className="myShareCountWrapper">{shareCount ? shareCount : null}</span>
+                );
+            }}
+        </FacebookShareCount>
+    );
 
-  return (
-    <div className="post-social">
-      <FacebookShareButton
-        quote={title}
-        url={url}
-        hashtags={hashTags}
-      >
-        <FacebookIcon
-          size={32}
-          round={true}
-        />
-        <div className="SocialMediaShareTitle">Facebook</div>
-        {fbShareCount}
-      </FacebookShareButton>
+    return (
+        <div className="post-social">
+            <FacebookShareButton
+                quote={title}
+                url={url}
+                hashtags={hashTags}
+            >
+                <FacebookIcon
+                    size={32}
+                    round={true}
+                />
+                <div className="SocialMediaShareTitle">Facebook</div>
+                {fbShareCount}
+            </FacebookShareButton>
 
-      <TwitterShareButton
-        title={title}
-        url={url}
-        via={socialConfig.twitterHandle.split("@").join("")}
-        hashtags={hashTags}
-      >
-        <TwitterIcon size={32} round={true}/>
-        <div className="SocialMediaShareTitle">Twitter</div>
-      </TwitterShareButton>
-    </div>
-  )
-}
+            <TwitterShareButton
+                title={title}
+                url={url}
+                via={socialConfig.twitterHandle.split('@').join('')}
+                hashtags={hashTags}
+            >
+                <TwitterIcon size={32} round={true}/>
+                <div className="SocialMediaShareTitle">Twitter</div>
+            </TwitterShareButton>
+        </div>
+    );
+};
 
-export default Share
+export default Share;
 
 /*
 * INFO:
