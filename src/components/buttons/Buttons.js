320870b90e01748a4db2283b09afdbb9bcdd639e
andris.svarcs <andris.svarcs@yahoo.com>
2019-10-15 07:14:24 +0100

    Formatting

--- src/components/buttons/Buttons.js
+++ src/components/buttons/Buttons.js
@@ -1,38 +1,38 @@
-import React from "react";
+import React from 'react';
 // import PropTypes from "prop-types";
 import styles from './Buttons.module.scss';
 
-export const Buttons = (props) => (
-  <button
-    className={styles.button}
-    type="button"
-  >
-    {props.children}
-  </button>
-)
+export const Buttons         = (props) => (
+    <button
+        className={styles.button}
+        type="button"
+    >
+        {props.children}
+    </button>
+);
 //https://migraine-detective.com/open-diary
 export const WhiteBlueButton = (props) => (
-  <a
-    className={styles.openYourDiaryButton}
-    href={props.href}
-  >
-    <span>{props.children}</span>
-  </a>
-)
+    <a
+        className={styles.openYourDiaryButton}
+        href={props.href}
+    >
+        <span>{props.children}</span>
+    </a>
+);
 
 export const BlackWhiteButton = (props) => (
-  <a
-    className={styles.blackWhiteButton}
-    href={props.href}
-  >
-    <span>{props.children}</span>
-  </a>
-)
+    <a
+        className={styles.blackWhiteButton}
+        href={props.href}
+    >
+        <span>{props.children}</span>
+    </a>
+);
 
 export const InputWhiteBlueButton = (props) => (
-  <input
-    className={styles.InputWhiteBlueButton}
-    type="submit"
-    value={props.value}
-  />
-)
+    <input
+        className={styles.InputWhiteBlueButton}
+        type="submit"
+        value={props.value}
+    />
+);
