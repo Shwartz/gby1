decf8c2b7a0a6d4ae11c0f2572ce06c80d9f10b9
andris.svarcs <andris.svarcs@yahoo.com>
2019-10-16 07:41:52 +0100

    Contact form send button size and forms colours

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,41 +1,41 @@
-import React from "react"
-import styles from "./ContactForm.module.scss"
-import {InputWhiteBlueButton} from "../buttons/Buttons"
+import React from 'react';
+import styles from './ContactForm.module.scss';
+import {InputWhiteBlueButton} from '../buttons/Buttons';
 
 const ContactForm = (props) => {
-  // expected props theme: light | dark
-  const theme = props.theme === 'light' ? styles.light : '';
+    // expected props theme: light | dark
+    const theme = props.theme === 'light' ? styles.light : '';
 
-  return (
-    <div className={`${styles.wrap} ${theme}`}>
-      <form
-        action="https://formspree.io/svarcs.01@gmail.com"
-        method="POST"
-      >
-        <label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Your email"
-          />
-        </label>
-        <label>
+    return (
+        <div className={`${styles.wrap} ${theme}`}>
+            <form
+                action="https://formspree.io/svarcs.01@gmail.com"
+                method="POST"
+            >
+                <label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Your email"
+                    />
+                </label>
+                <label>
         <textarea
-          name="message"
-          placeholder="Your message"
+            name="message"
+            placeholder="Your message"
         />
-        </label>
-        <label>
-          <InputWhiteBlueButton
-            value="Send"
-          />
-        </label>
-      </form>
-    </div>
-  )
-}
+                </label>
+                <label>
+                    <InputWhiteBlueButton
+                        value="Send"
+                    />
+                </label>
+            </form>
+        </div>
+    );
+};
 
-export default ContactForm
+export default ContactForm;
 
 
 
